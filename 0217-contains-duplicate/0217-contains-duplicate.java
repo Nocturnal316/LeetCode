beacb10bebfdddefc61c0d8659d0d57b17527ccf
Nocturnal316 <6785862+Nocturnal316@users.noreply.github.com>
2023-02-06 11:03:28 -0800

    Time: 11 ms (61.13%), Space: 54.9 MB (63.40%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<Integer>();
+        for(int a:  nums){
+            if(set.contains(a)) return true;
+            set.add(a);
+        }
+        return false;
+    }
+}
