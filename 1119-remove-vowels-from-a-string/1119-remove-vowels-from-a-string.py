d9992f8f15d4601a6b7876b7c07eb5e67eaf8a9e
Nocturnal316 <6785862+Nocturnal316@users.noreply.github.com>
2023-02-06 10:29:47 -0800

    Time: 37 ms (40.47%), Space: 13.8 MB (49.06%) - LeetHub

--- 1119-remove-vowels-from-a-string/1119-remove-vowels-from-a-string.py
+++ 1119-remove-vowels-from-a-string/1119-remove-vowels-from-a-string.py
@@ -1,8 +1,8 @@
 class Solution:
     def removeVowels(self, s: str) -> str:
         vowels = {'a','e','i','o', 'u'}
-        ans = ''
+        ans = []
         for letter in s:
             if not letter in vowels:
-                ans += letter
-        return ans
+                ans.append(letter)
+        return ''.join(ans)
