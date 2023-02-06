7349db55ee87e3952be1dcbf04c8a283bea55b80
Nocturnal316 <6785862+Nocturnal316@users.noreply.github.com>
2023-02-06 11:15:36 -0800

    Time: 593 ms (32.82%), Space: 31.4 MB (5.52%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -6,5 +6,14 @@ class Solution:
 #                 return True
 #             numSet.add(num)
 #         return False
-        return len(nums) > len(set(nums))
+
+
+        # return len(nums) > len(set(nums))
         
+        hash_map = dict()
+        for i in nums:
+            if  i in hash_map:
+                 return True
+            else:
+                 hash_map[i] =1
+        return False
