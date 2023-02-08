e992c270c4344a2129a90fe06f86494126a9abc6
Nocturnal316 <6785862+Nocturnal316@users.noreply.github.com>
2023-02-08 11:43:02 -0800

    Time: 39 ms (15.09%), Space: 46.1 MB (45.43%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        
+        HashMap<String,List> map = new HashMap<>();
+        
+        for(String string : strs){
+            char[] temp = string.toCharArray();
+            Arrays.sort(temp);
+            System.out.println(String.valueOf(temp));
+            String sortedWord =  String.valueOf(temp);
+            if(!map.containsKey(sortedWord)){
+                map.put(sortedWord,new ArrayList());
+            }
+            map.get(sortedWord).add(string);
+        }
+        
+        
+        
+        return new ArrayList(map.values());
+        
+    }
+}
