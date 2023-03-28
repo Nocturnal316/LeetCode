0471e20bcfd9d05b942ca8802c4d0fe0e1045e11
Nocturnal316 <6785862+Nocturnal316@users.noreply.github.com>
2023-03-27 22:25:57 -0700

    Time: 14 ms (19.19%), Space: 42.7 MB (45.17%) - LeetHub

---
+++ 0136-single-number/0136-single-number.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        
+        for(int num: nums){
+            if(set.contains(num)){
+               set.remove(num); 
+            }else{
+                set.add(num);
+            }
+            
+        }
+        
+        return set.stream().findFirst().get();
+    }
+}
