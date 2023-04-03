790ddabcdc680dabf9ad3c8480194e20c89035a3
Nocturnal316 <6785862+Nocturnal316@users.noreply.github.com>
2023-04-03 15:58:57 -0700

    Time: 4 ms (6.19%), Space: 42.6 MB (19.64%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int row = matrix.length;
+        int col = matrix[0].length;
+        
+        int left = 0;
+        int right = row*col-1;
+        
+        while(left <= right){
+            int mid = left+(right-left)/2;
+           
+            int num = matrix[mid/col][mid%col];
+            System.out.println(num);
+            if(num == target){
+                return true;
+            }else if(num > target){
+                right = mid-1;
+            }else{
+                left = mid +1;
+            }
+            
+        }
+        
+        return false;
+    }
+}
