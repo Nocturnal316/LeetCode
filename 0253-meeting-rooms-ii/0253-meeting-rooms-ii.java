62b51c6b53af2c116a08b1ba5e971390e2f264ee
Nocturnal316 <6785862+Nocturnal316@users.noreply.github.com>
2023-04-05 16:48:08 -0700

    Time: 15 ms (8.09%), Space: 43.4 MB (12.99%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.java
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.java
@@ -2,13 +2,29 @@ class Solution {
     
     public int minMeetingRooms(int[][] intervals) {
         //line sweep
-          Arrays.sort(intervals, (i1, i2) -> i1[0] - i2[0]);    
-    var heap = new PriorityQueue<int[]>((i1, i2) -> i1[1] - i2[1]);
-    for (var meeting: intervals) {
-      heap.offer(meeting);
-      if (heap.peek()[1] <= meeting[0]) heap.poll();      
-    }
-    return heap.size();
-  
+        TreeMap<Integer,Integer> destruct = new TreeMap<>();
+        for(int[] interval : intervals){
+            destruct.put(interval[0],destruct.getOrDefault(interval[0],0)+1);
+            destruct.put(interval[1],destruct.getOrDefault(interval[1],0)-1);
+        }
+        int cur=0;
+        int ans=0;
+        
+        for(Integer p : destruct.values()){
+            cur += p;
+            ans = Math.max(ans,cur);
+        }
+        return ans;
+        /**
+        *   Arrays.sort(intervals, (i1, i2) -> i1[0] - i2[0]);    
+            var heap = new PriorityQueue<int[]>((i1, i2) -> i1[1] - i2[1]);
+            for (var meeting: intervals) {
+              heap.offer(meeting);
+              if (heap.peek()[1] <= meeting[0]) heap.poll();      
+            }
+            return heap.size();
+        }*/
+        
+        
     }
 }
