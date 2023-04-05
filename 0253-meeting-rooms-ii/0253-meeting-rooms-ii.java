d8b64c3cfee53ae72619cf29171270bae0208ad9
Nocturnal316 <6785862+Nocturnal316@users.noreply.github.com>
2023-04-05 16:41:48 -0700

    Time: 25 ms (5.00%), Space: 44.3 MB (6.35%) - LeetHub

---
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.java
@@ -0,0 +1,21 @@
+class Solution {
+    
+    public int minMeetingRooms(int[][] intervals) {
+        //line sweep
+        
+        TreeMap<Integer,Integer> destruct = new TreeMap<>();
+        for(int[] interval : intervals){
+            destruct.put(interval[0],destruct.getOrDefault(interval[0],0)+1);
+            destruct.put(interval[1],destruct.getOrDefault(interval[1],0)-1);
+        }
+        int cur=0;
+        int ans=0;
+        System.out.println(destruct);
+        
+        for(Integer p : destruct.values()){
+            cur += p;
+            ans = Math.max(ans,cur);
+        }
+        return ans;
+    }
+}
