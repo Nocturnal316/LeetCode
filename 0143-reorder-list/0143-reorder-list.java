12bc60e25ff6ce9f12e5f85b85c5d1b62f333a85
Nocturnal316 <6785862+Nocturnal316@users.noreply.github.com>
2023-04-05 17:55:29 -0700

    Time: 1 ms (100.00%), Space: 45.3 MB (61.12%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.java
+++ 0143-reorder-list/0143-reorder-list.java
@@ -10,44 +10,38 @@
  */
 class Solution {
     public void reorderList(ListNode head) {
-        ListNode fast = head;
-        ListNode mid = head;
-        ListNode prev = head;
         
-        while(fast!=null && fast.next != null){
-            prev = mid;
-            fast = fast.next.next;
-            mid = mid.next;
+        
+        ListNode mergedHead = head;
+        ListNode midPoint = head;
+        ListNode endPoint = head;
+        
+        while(endPoint != null && endPoint.next != null){
+            midPoint = midPoint.next;
+            endPoint = endPoint.next.next;
         }
         
-        prev.next = null;
-        prev = null;
-        ListNode next;
-        while(mid != null ){
-            next = mid.next;
-            mid.next = prev;
-            prev = mid;
-            mid = next;
+        ListNode prev = null;
+        ListNode cur = midPoint.next;
+        midPoint.next = null;
+        
+        while(cur != null){
+            ListNode temp = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = temp;
         }
         
-        ListNode l1 = head;
-        ListNode l2 = prev;
-        
-        while(l1!= null & l2 !=null){
-            ListNode temp = l1.next;
-            ListNode temp2 = l2.next;
-            l1.next = l2;
-            l2 = temp2;
-            l1 = l1.next;
-            l1.next = temp;
-            if(l1.next == null){
-                l1.next = l2;
-                break;
-            }
-            l1 = l1.next;
-        
+        while(prev != null){
+            ListNode temp = mergedHead.next;
+            ListNode temp2 =  prev.next;
+            mergedHead.next = prev;
+            mergedHead.next.next = temp;
+            mergedHead = mergedHead.next.next;
+            prev = temp2;
             
         }
- 
+        
+    
     }
 }
