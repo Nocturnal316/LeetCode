9b98eaa00208b3b528ebbf14dcf5164eda2f9aab
Nocturnal316 <6785862+Nocturnal316@users.noreply.github.com>
2023-04-11 16:12:10 -0700

    Time: 1 ms (78.24%), Space: 39.7 MB (74.45%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.java
+++ 0377-combination-sum-iv/0377-combination-sum-iv.java
@@ -1,15 +1,17 @@
 class Solution {
     public int combinationSum4(int[] nums, int target) {
-        int[] dp = new int[nums.length+1];
-        HashMap<Integer,Integer> map = new HashMap<>();
-        map.put(0,1);
-        for(int total = 1; total <= target; total++){
-            map.put(total,0);
+        int[] dp = new int[target+1];
+        dp[0] = 1;
+        
+        for(int total = 1; total <=target; total++){
             for(int num : nums){
-                map.put(total,map.get(total)+map.getOrDefault(total-num,0));
+                if(total >=num){
+                    dp[total] += dp[total-num];
+                }
             }
         }
+     
         
-        return map.get(target);
+        return dp[target];
     }
 }
