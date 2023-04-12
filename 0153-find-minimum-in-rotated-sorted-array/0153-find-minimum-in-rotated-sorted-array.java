f5001920c05accbe04dc06a1fcdd3e306ade10d1
Nocturnal316 <6785862+Nocturnal316@users.noreply.github.com>
2023-04-11 18:21:05 -0700

    Time: 0 ms (100.00%), Space: 42.4 MB (34.10%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
@@ -6,7 +6,6 @@ class Solution {
         int ans;
         while(lo < hi){
             int mid = lo + (hi-lo) /2;
-            
             if(nums[mid] > nums[hi]){
                 lo = mid+1;
             }else{
