870daa7d2a24a3748a2f1a97d682fad5a135d714
Nocturnal316 <6785862+Nocturnal316@users.noreply.github.com>
2023-04-12 13:23:28 -0700

    Time: 3 ms (15.48%), Space: 55.8 MB (96.41%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,12 +1,14 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        if(prices.length == 0) return 0;
         int[] dp = new int[prices.length];
         dp[0] = 0;
         int minVal = prices[0];
         for(int i = 1; i < prices.length;i++){
-            if(minVal > prices[i]) minVal = prices[i];
+            if(minVal > prices[i]){
+                minVal = prices[i];
+            }
             dp[i] = Math.max(dp[i-1],prices[i] - minVal);
+            
         }
         return dp[prices.length-1];
     }
