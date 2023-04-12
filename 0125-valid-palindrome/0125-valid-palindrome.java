7a48e023b31b42eb2d23d39c51a6fb4f2a6944fc
Nocturnal316 <6785862+Nocturnal316@users.noreply.github.com>
2023-04-12 13:44:21 -0700

    Time: 4 ms (69.24%), Space: 43 MB (53.99%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,19 +1,18 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        
-        String temp = s.toLowerCase().replaceAll(" ","");
-        char[] chart = temp.toCharArray();
+        String lower = s.toLowerCase();
+        char[] chars = lower.toCharArray();
+        if(lower.length() == 1) return true;
         int left = 0;
-        int right = chart.length-1;
-        
+        int right = chars.length-1;
         while(left < right){
-            while( left  < right && !Character.isLetterOrDigit(chart[left])) left++;
-            while( left < right  && !Character.isLetterOrDigit(chart[right])) right--;
-            if( left > right  || chart[left] != chart[right]) return false;
+            while(left < right && !Character.isLetterOrDigit(chars[left])) left++;
+            while(left < right && !Character.isLetterOrDigit(chars[right])) right--;
+            if(left > right || chars[left] != chars[right]) return false;
             left++;
             right--;
         }
-            
-        return true;
+        
+        return true ;
     }
 }
