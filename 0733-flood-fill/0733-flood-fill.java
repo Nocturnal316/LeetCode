2a202af5cb5edbd5db86a7f001f39fe30b8ba829
Nocturnal316 <6785862+Nocturnal316@users.noreply.github.com>
2023-04-13 16:51:43 -0700

    Time: 2 ms (12.06%), Space: 43.8 MB (17.71%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -16,7 +16,7 @@ class Solution {
             for(int i = 0; i < dir.length; i+=2 ){
                 int row = r+dir[i];
                 int col = c+dir[i+1];
-                if(row>=0 && row<m && col >= 0 && col < n && !fillSeen[row][col]){
+                if(row >= 0 && row < m && col >= 0 && col < n && !fillSeen[row][col]){
                     if(image[r][c] == image[row][col]){
                         queue.offer(new Pair<>(row,col));
                     }
