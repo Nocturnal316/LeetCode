a8f5edae431865e26715404f2cee85667ed599ce
Nocturnal316 <6785862+Nocturnal316@users.noreply.github.com>
2023-04-17 12:02:50 -0700

    Time: 5 ms (91.55%), Space: 43.4 MB (48.31%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.java
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.java
@@ -9,13 +9,29 @@
  */
 
 class Solution {
+    // public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+    //     if(root.val < p.val && root.val < q.val){
+    //         return lowestCommonAncestor(root.right,p,q);
+    //     }else if(root.val > p.val && root.val > q.val){
+    //         return lowestCommonAncestor(root.left,p,q);
+    //     }else{
+    //         return root;
+    //     }
+    // }
+    
+    
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if(root.val < p.val && root.val < q.val){
-            return lowestCommonAncestor(root.right,p,q);
-        }else if(root.val > p.val && root.val > q.val){
-            return lowestCommonAncestor(root.left,p,q);
-        }else{
-            return root;
+       TreeNode cur = root;
+        
+        while(cur != null){
+            if(cur.val < p.val && cur.val < q.val){
+                cur = cur.right;
+            }else if(cur.val > p.val && cur.val > q.val){
+                cur = cur.left;
+            }else{
+                return cur;
+            }
         }
+        return cur;
     }
 }
