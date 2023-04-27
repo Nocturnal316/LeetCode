ca6b7a8fbecfadd3115e6c4ab41aa6086bb3c21c
Nocturnal316 <6785862+Nocturnal316@users.noreply.github.com>
2023-04-27 12:57:17 -0700

    Time: 0 ms (100.00%), Space: 42.5 MB (43.58%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
@@ -14,19 +14,21 @@
  * }
  */
 class Solution {
-    int path =  0;
+    int diameter = 0;
     
-    public int helper(TreeNode root){
-        if(root == null) return 0;
-        int left = helper(root.left);
-        int right = helper(root.right) ;
+    public int countDiameter(TreeNode root){
+        if(root == null) return 0 ;
         
-        path = Math.max(path, left+right);
-      
-        return Math.max(left,right) +1;
+        int l = countDiameter(root.left);
+        int r = countDiameter(root.right);
+        diameter = Math.max(diameter,l+r);
+        
+        return Math.max(l, r)+1;
     }
+    
     public int diameterOfBinaryTree(TreeNode root) {
-        helper(root);
-        return path;
+        
+        countDiameter(root);
+        return diameter;
     }
 }
