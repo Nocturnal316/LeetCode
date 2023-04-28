4b27530f90ab663fab30e6a6371982403bf1efa8
Nocturnal316 <6785862+Nocturnal316@users.noreply.github.com>
2023-04-27 18:08:21 -0700

    Time: 5 ms (59.70%), Space: 43.4 MB (16.30%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,18 +1,15 @@
 class Solution {
-    public boolean canConstruct(String ransomNote, String magazine) {
-        
+    public boolean canConstruct(String ransomNote, String magazine) { 
         int[] magazineInventory = new int[26];
         
         for(Character c : magazine.toCharArray()){
             magazineInventory[c-'a']++;
         }
         
-        
         for(Character c : ransomNote.toCharArray()){
             if(magazineInventory[c-'a'] - 1 < 0) return false;
             magazineInventory[c-'a']--;
         }
-        
         return true;
     }
 }
