966f3fc305123d00308fb8551816215c6281849f
Nocturnal316 <6785862+Nocturnal316@users.noreply.github.com>
2023-05-01 12:38:17 -0700

    Time: 2 ms (45.18%), Space: 44.3 MB (8.77%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -2,18 +2,16 @@ class Solution {
     public void moveZeroes(int[] nums) {
         if(nums.length == 1) return;
         int left = 0;
-      
-        
         for(int right = 1; right < nums.length;right++){
+            //lets move left to the first non zero
             while(nums[left] != 0 && left < right){
                 left++;
             }
-            
+            //swap if right isn't 0 and left is
             if(nums[right] != 0 && nums[left] == 0){
                 nums[left] = nums[right];
                 nums[right] = 0;
             }
         }
-    
     }
 }
