7de5e487251aea63bd78f2dccf86cc75abe484e8
Nocturnal316 <6785862+Nocturnal316@users.noreply.github.com>
2023-05-01 13:36:21 -0700

    Time: 4 ms (23.21%), Space: 42 MB (16.43%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -0,0 +1,64 @@
+class Solution {
+    class TrieNode{
+        private HashMap<Character,TrieNode> children;
+        private boolean isWord;
+        public Character letter;
+        
+         public TrieNode(){
+            isWord = false;
+            children = new HashMap<>();
+            letter = null;
+        }
+          public TrieNode(Character ch){
+            isWord = false;
+            children = new HashMap<>();
+            letter = ch;
+        }
+    }
+    
+    
+    class Trie{
+        TrieNode root;
+        
+        public Trie(){
+            root = new TrieNode();
+        }
+        
+        public void insert(String s){
+            TrieNode cur = root;
+            
+            for(Character ch : s.toCharArray()){
+                if(!cur.children.containsKey(ch)){
+                    cur.children.put(ch,new TrieNode(ch));
+                }
+                cur = cur.children.get(ch);
+            }
+            
+            cur.isWord = true;
+        }
+        
+        public String count(String s){
+            TrieNode cur = root;
+            StringBuilder sb = new StringBuilder();
+            
+            for(Character ch : s.toCharArray()){
+                if(cur.children.size() != 1 || cur.isWord ) return sb.toString();
+                cur = cur.children.get(ch);
+                sb.append(ch);
+            }
+            return sb.toString();
+        }
+    }
+    public String longestCommonPrefix(String[] strs) {
+        
+        Trie root = new Trie();
+        
+        for(String s : strs){
+            root.insert(s);
+        }
+        
+    
+        
+        return    root.count(strs[0]);
+    }
+}
