483cc7abb38ea4e91728736d8351232b79d67f9a
Nocturnal316 <6785862+Nocturnal316@users.noreply.github.com>
2023-05-02 12:10:47 -0700

    Time: 3 ms (94.09%), Space: 51.4 MB (39.18%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -3,9 +3,8 @@ class Solution {
     int m;
     int n;
     public int numIslands(char[][] grid) {
-    
-         m = grid.length;
-         n = grid[0].length;
+        m = grid.length;
+        n = grid[0].length;
         int count = 0;
         for(int row = 0; row < m; row++){
             for(int col = 0; col < n; col++){
@@ -20,7 +19,6 @@ class Solution {
     
     public void dfs(char[][] grid, int row, int col){
         grid[row][col] = 2;
-        
         for(int[] dir : dirs){
             int newR = row + dir[0];
             int newC = col + dir[1];
