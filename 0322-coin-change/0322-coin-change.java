31d1be1811b292277ac96e801335e8f93f48c04d
Nocturnal316 <6785862+Nocturnal316@users.noreply.github.com>
2023-05-02 15:59:01 -0700

    Time: 475 ms (5.08%), Space: 50.4 MB (5.06%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -11,17 +11,18 @@ class Solution {
         //for all the coints in the bag and the current target amount
         for(int coin : coins){
             //if subtracting would make it negative skip
-            if(amount - coin < 0) continue;
+            int remain = amount - coin;
+            if(remain < 0) continue;
             //if we didn calculate yet. do so
-            if(!memo.containsKey(amount-coin)) coinChange(coins,amount-coin);
+            if(!memo.containsKey(remain)) coinChange(coins,remain);
             //if the previous sub problem has been solved;
-            if(memo.get(amount - coin) != -1){
+            if(memo.get(remain) != -1){
                 //update the current problem with the minum
                 // of the keeping same amount or takes previosu sub problem + 1;
                 // it might not have been solved yet so use Intermagx val
                 // increase by 1;
                 memo.put(amount, Math.min(memo.get(amount),
-                        memo.getOrDefault(amount-coin,Integer.MAX_VALUE)+1));
+                        memo.getOrDefault(remain,Integer.MAX_VALUE)+1));
             }
            
         }
