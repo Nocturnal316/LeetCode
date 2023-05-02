18325d473552a97ff223b13d9fa063e2886d8ea9
Nocturnal316 <6785862+Nocturnal316@users.noreply.github.com>
2023-05-02 15:45:11 -0700

    Time: 486 ms (5.08%), Space: 50.5 MB (5.06%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -2,21 +2,23 @@ class Solution {
     HashMap<Integer,Integer> memo = new HashMap<>();
     
     public int coinChange(int[] coins, int amount) {
-        if(amount == 0) {
+        if(amount == 0){
             memo.put(0,0);
             return 0;
         }
+        
         memo.put(amount,Integer.MAX_VALUE);
+        
         for(int coin : coins){
             if(amount - coin < 0) continue;
             if(!memo.containsKey(amount-coin)) coinChange(coins,amount-coin);
-       
-            if(memo.get(amount-coin)!= -1){
-                memo.put(amount,Math.min(memo.get(amount),
-                                  memo.getOrDefault(amount-coin,Integer.MAX_VALUE)+1));
+            if(memo.get(amount - coin) != -1){
+                memo.put(amount, Math.min(memo.get(amount),
+                        memo.getOrDefault(amount-coin,Integer.MAX_VALUE)+1));
             }
-            
+           
         }
+        
         if(memo.get(amount) == Integer.MAX_VALUE)   memo.put(amount, -1);
         
         return memo.get(amount);
