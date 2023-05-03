8c00fc45cb080d332c2fb1b36531f290653913de
Nocturnal316 <6785862+Nocturnal316@users.noreply.github.com>
2023-05-03 13:35:40 -0700

    Time: 2 ms (85.38%), Space: 42.2 MB (61.98%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -10,6 +10,9 @@ class Solution {
         
         for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
+                //becareful of logic
+                //at first i counted all oranges and check
+                // to see if fresh == rotten.
                 if(grid[i][j] == 2){
                     queue.offer(new int[]{i,j});
                 }
@@ -19,16 +22,17 @@ class Solution {
                 }
             }
         }
-          if (numOranges == 0) {
+        
+        //base case if there are no fresh oranges 
+        if (numOranges == 0) {
             return 0;
         }
 
         while(!queue.isEmpty()){
+            //level = minute 
             int size = queue.size();
-            
             for(int i = 0; i < size; i++){
                 int[]  pos = queue.poll();
-               
                 int curR = pos[0];
                 int curC = pos[1];
                 for(int[] dir: dirs){
@@ -36,15 +40,14 @@ class Solution {
                     int newC = curC +dir[1];
                     if(newR < 0 || newR >= m || newC < 0 || newC >= n 
                        || grid[newR][newC] == 0 || grid[newR][newC] == 2 ) continue;
-                    
                     queue.offer(new int[]{newR,newC});
                     grid[newR][newC] = 2;
-                     numOranges-=1;
+                    numOranges-=1;
                 }
             }
             level++;
         }
-        
+        //last orange may have caused level increase to substract by 1
         return numOranges == 0 ? level-1 : -1;
     }
 }
