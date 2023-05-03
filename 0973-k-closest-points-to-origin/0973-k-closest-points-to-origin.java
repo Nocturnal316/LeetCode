eb1e7d7f1869aa34adf04e0ca69d782e1889f7f7
Nocturnal316 <6785862+Nocturnal316@users.noreply.github.com>
2023-05-03 12:52:17 -0700

    Time: 30 ms (70.72%), Space: 50.9 MB (64.78%) - LeetHub

---
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
@@ -0,0 +1,34 @@
+class Solution {
+     class Point{
+        int x;
+        int y;
+        int dist;
+        public Point(int x, int y){
+            this.x = x;
+            this.y = y;
+        }
+        
+        public int getDist(){
+            return ((0-x)*(0-x) + (0-y)*(0-y));
+        }
+        
+        public int[] getList(){
+            return new int[]{this.x,this.y};
+        }
+    }
+    public int[][] kClosest(int[][] points, int k) {
+        PriorityQueue<Point> que = new PriorityQueue<>((a,b)->a.getDist() - b.getDist());
+        
+        for(int[] point : points){
+            que.add(new Point(point[0],point[1]));
+        }
+        ArrayList<int[]> ans = new ArrayList<>();
+        for(int i  = 0; i < k;i++){
+            Point p = que.poll();
+            ans.add(new int[]{p.x,p.y});
+        }
+       
+        
+        return ans.toArray(new int[ans.size()][]);
+    }
+}
