66f10c2d8569509f617cbda0a306f70ed2025bae
Nocturnal316 <6785862+Nocturnal316@users.noreply.github.com>
2023-05-04 11:27:10 -0700

    Time: 1321 ms (5.43%), Space: 132.6 MB (6.20%) - LeetHub

---
+++ 2400-number-of-ways-to-reach-a-position-after-exactly-k-steps/2400-number-of-ways-to-reach-a-position-after-exactly-k-steps.java
@@ -0,0 +1,16 @@
+class Solution {
+    int mod = (int) Math.pow(10,9)+7;
+    HashMap<String,Integer> memo = new HashMap<>();
+    public int numberOfWays(int startPos, int endPos, int k) {
+        if(startPos == endPos &&  k == 0 ) return 1;
+        if(k == 0) return 0;
+        String combo = startPos+"_"+k;
+        //int combo = Math.abs(startPos-endPos);
+        if(memo.containsKey(combo)) return memo.get(combo);
+        int left = numberOfWays(startPos - 1, endPos,  k - 1)%mod;
+        int right = numberOfWays(startPos + 1, endPos,  k - 1)%mod;
+        
+        memo.put(combo,(left+right)%mod);
+        return memo.get(combo);
+    }
+}
