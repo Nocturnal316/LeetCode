419886304a723d697c201e9663202dc2e9798cb5
Nocturnal316 <6785862+Nocturnal316@users.noreply.github.com>
2023-05-05 01:05:17 -0700

    Time: 8 ms (5.01%), Space: 42.2 MB (5.40%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,22 +1,22 @@
 class Solution {
     HashMap<String,Integer> memo = new HashMap<>();
     public int uniquePaths(int m, int n) {
-      
         return uniquePath(m-1,n-1);
     }
-    
+    //Summation DP
     public int uniquePath(int m, int n){
+        //hit goal return 1
         if(m == 0 && n == 0) return 1;
+        //invalid move return 0
         if(m < 0 || n < 0) return 0;
         String curPos = m+"_"+n;
-        
+        //if calulated return prev calculation
         if(memo.containsKey(curPos)) return memo.get(curPos);
         
         int moveLeft = uniquePath(m-1,n);
         int moveUp = uniquePath(m,n-1);
         
         memo.put(curPos,moveLeft+moveUp);
-        
         return moveLeft+moveUp;
     }
 }
