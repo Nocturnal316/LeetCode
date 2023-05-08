e246f8b85b4a8213d05bb52bd74c2574ce08a8a9
Nocturnal316 <6785862+Nocturnal316@users.noreply.github.com>
2023-05-08 13:07:02 -0700

    Time: 452 ms (10.17%), Space: 40.8 MB (46.94%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,74 +1,56 @@
 class Solution {
     int[][] directions = {{0,1},{1,0},{-1,0},{0,-1}};
-    TrieNode root;
-    public class TrieNode{
-        Boolean isWord;
+    TrieNode root = new TrieNode();
+    class TrieNode{
         HashMap<Character,TrieNode> children;
-        String name;
-        Boolean visited;
-        int r, c;
-        
-        public TrieNode(){
-            isWord = false;
+        Boolean isWord;
+        TrieNode(){
             children = new HashMap<>();
-            visited = false;
-        }
-        
-        public TrieNode(int r, int c){
-            this.r =r;
-            this.c =c;
             isWord = false;
-            children = new HashMap<>();
-            visited = false;
-        }
-        
-        public String toString(){
-            return name;
         }
     }
     
-     public void insertWord(String word) {
-        TrieNode curr = root;
+    public void insertWord(String word){
+        TrieNode cur = root;
         for(Character c : word.toCharArray()){
-            if(!curr.children.containsKey(c)){
-                curr.children.put(c,new TrieNode());
+            if(!cur.children.containsKey(c)){
+                cur.children.put(c, new TrieNode());
             }
-            curr =  curr.children.get(c);
+            cur = cur.children.get(c);
         }
-        curr.isWord = true;
+        cur.isWord = true;
     }
-    
     public boolean exist(char[][] board, String word) {
-        int m = board.length;
-        int n = board[0].length;
-        
-        root = new TrieNode();
         insertWord(word);
         
-        for(int row = 0; row < m;row++){
-            for(int col = 0; col < n; col++){
-                if(dfs(root,board,row,col))return true;
+        int m = board.length;
+        int n = board[0].length;
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(dfs(board,root,i,j)) return true;
             }
         }
-    
         return false;
     }
     
-    public boolean dfs(TrieNode root,char[][] board, int i, int j){
-        if(root.isWord) return root.isWord;
+    
+    public boolean dfs(char[][] board, TrieNode root, int m, int n){
+        if(root.isWord) return true;
         
-        if(i<0 || j < 0 || i >= board.length || j >= board[0].length ||
-          !root.children.containsKey(board[i][j])) return false;
         
-        Character temp = board[i][j];
-        board[i][j] =  ' ';
-       
-        TrieNode child = root.children.get(temp);
-        if(dfs(child,board,i+1,j)||dfs(child,board,i-1,j)
-           ||dfs(child,board,i,j+1)||dfs(child,board,i,j-1)) return true;
-        board[i][j]=temp;
+        if(m < 0 || m >= board.length || n < 0 
+           || n >= board[0].length || !root.children.containsKey(board[m][n])) return false;
         
+        Character temp = board[m][n];
+        board[m][n] =  ' ';
+        TrieNode cur = root.children.get(temp);
+        for(int[] dir : directions){
+            int newR = m + dir[0];
+            int newC = n + dir[1];
+            if(dfs(board,cur,newR,newC)) return true;
+        }
+        
+        board[m][n] =  temp;
         return false;
     }
-    
 }
