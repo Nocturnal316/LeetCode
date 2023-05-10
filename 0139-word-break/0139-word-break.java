90106dde8df46ff754933b0470ffadce47b2ac5b
Nocturnal316 <6785862+Nocturnal316@users.noreply.github.com>
2023-05-09 19:15:44 -0700

    Time: 39 ms (6.12%), Space: 43.7 MB (9.13%) - LeetHub

---
+++ 0139-word-break/0139-word-break.java
@@ -0,0 +1,54 @@
+class Solution {
+    TrieNode root = new TrieNode();
+    HashSet<Integer> indexSeen = new HashSet<>();
+          
+    class TrieNode{
+        Boolean isWord;
+        HashMap<Character, TrieNode> children;
+        
+        public TrieNode(){
+            isWord = false;
+            children = new HashMap<>();
+        }
+    }
+     
+    public void insertWord(String word){
+        TrieNode cur = this.root;
+        for(Character c : word.toCharArray()){
+            if(!cur.children.containsKey(c)){
+                cur.children.put(c,new TrieNode());
+            }
+            cur = cur.children.get(c);
+        }
+        cur.isWord = true;
+    }
+    
+    public boolean wordBreak(String s, List<String> wordDict) {
+        for(String word : wordDict){
+            insertWord(word);
+        }
+        
+        return dfsBreak(s,root,0);
+    }
+    
+    public boolean dfsBreak(String s, TrieNode parent, int pos){
+        if(pos  == s.length() ) return true;
+    
+        if(indexSeen.contains(pos))return false;
+        indexSeen.add(pos);
+        TrieNode cur = parent;
+        for(int i = pos; i < s.length();i++){
+            Character c = s.charAt(i);
+           
+            cur = cur.children.get(c);
+           
+            if(cur == null) return false;
+            System.out.println(c);
+            System.out.println(i);
+            System.out.println(cur.isWord);
+            System.out.println(cur.children);
+            if(cur.isWord && dfsBreak(s,root,i+1)) return true;
+        }
+        return false;
+    }
+}
