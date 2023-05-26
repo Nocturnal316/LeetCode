dd1af340fb2de0d1f97f87e30e4e3be8f2745a82
Nocturnal316 <6785862+Nocturnal316@users.noreply.github.com>
2023-05-26 13:07:18 -0700

    Time: 0 ms (100.00%), Space: 39.5 MB (58.45%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.java
+++ 0367-valid-perfect-square/0367-valid-perfect-square.java
@@ -1,22 +1,22 @@
 class Solution {
-  public boolean isPerfectSquare(int num) {
-    if (num < 2) {
-      return true;
+    public boolean isPerfectSquare(int num) {
+        if(num < 2) return true;
+        long lo = 1;
+        long hi = num;
+        while(lo <= hi){
+            long mid = lo + (hi -lo) /2;
+            long sqrt = mid * mid;
+            if(sqrt == num){
+                return true;
+            }
+            else if(sqrt > num ){
+                hi = mid - 1;
+            }else{
+                lo = mid + 1;
+                
+            }
+            if(lo > hi) return false;
+        }
+        return false;
     }
-
-    long left = 2, right = num / 2, x, guessSquared;
-    while (left <= right) {
-      x = left + (right - left) / 2;
-      guessSquared = x * x;
-      if (guessSquared == num) {
-        return true;
-      }
-      if (guessSquared > num) {
-        right = x - 1;
-      } else {
-        left = x + 1;
-      }
-    }
-    return false;
-  }
 }
