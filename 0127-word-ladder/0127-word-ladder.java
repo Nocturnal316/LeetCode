1e1b4eeb2b97b22927dbb27c51a1fd1ec6b0a25f
Nocturnal316 <6785862+Nocturnal316@users.noreply.github.com>
2023-06-05 18:23:53 -0700

    Time: 142 ms (32.71%), Space: 47 MB (26.71%) - LeetHub

---
+++ 0127-word-ladder/0127-word-ladder.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        char[] ALPHABETS = new char[26];
+         for (int i = 0; i < 26; i++) {
+            ALPHABETS[i] = (char) (i + 'a');
+        }
+        HashSet<String> dict = new HashSet<>(wordList);
+        
+        if(!dict.contains(endWord)) return 0;
+        HashSet<String> seen = new HashSet<>();
+        seen.add(beginWord);
+        Queue<String> que = new LinkedList<>();
+        que.offer(beginWord);
+        int steps = 0;
+      
+        while(!que.isEmpty()){
+            int size = que.size();
+            steps+=1;
+            for(int i = 0; i < size;i++){
+                String node = que.poll();
+                for(int pos = 0; pos < beginWord.length();pos++){
+                    for(char c : ALPHABETS){
+                        StringBuilder sb = new StringBuilder(node);
+                        sb.setCharAt(pos,c);
+                        String newWord = sb.toString();
+                        if(dict.contains(newWord) && !seen.contains(newWord)){
+                            if(newWord.equals(endWord)) return steps + 1;
+                            seen.add(newWord);
+                            que.offer(newWord);
+                        }
+                    }
+                }
+            } 
+        }
+        return 0;
+    }
+}
