f6e327b2ac68502ac09aa33c0c903181f06876fd
Nocturnal316 <6785862+Nocturnal316@users.noreply.github.com>
2023-06-08 12:37:36 -0700

    Time: 22 ms (98.41%), Space: 46.3 MB (70.71%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -11,12 +11,16 @@ class Solution {
         }
     
         for(int i = 0; i<pref.length;i++){
-           
-            
+            //we have seen the compliment already
+            //which means we found the segment we can remove
+            // a[n]= a[compliment] + a[...n]
+            // a[6]= range[0,1]+ range[2,6]
             ans+= map.getOrDefault(pref[i]-k,0); 
             
+                //
             map.put(pref[i],map.getOrDefault(pref[i],0)+1);
         }
+        
         return ans;
         
     }
