1e337b9f780a675219273012657d1d8b617806c0
Nocturnal316 <6785862+Nocturnal316@users.noreply.github.com>
2023-06-08 21:49:38 -0700

    Time: 2 ms (93.74%), Space: 61.1 MB (28.17%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,14 +1,32 @@
 class Solution {
-    public int maxProfit(int[] prices) {
-        int[] dp = new int[prices.length];
-        dp[0] = 0;
-        int minVal = prices[0];
-        for(int i = 1; i < prices.length;i++){
-            if(minVal > prices[i]){
-                minVal = prices[i];
-            }
-            dp[i] = Math.max(dp[i-1],prices[i] - minVal);
-        }
-        return dp[prices.length-1];
+//     public int maxProfit(int[] prices) {
+//         int[] dp = new int[prices.length];
+//         dp[0] = 0;
+//         int minVal = prices[0];
+//         for(int i = 1; i < prices.length;i++){
+//             if(minVal > prices[i]){
+//                 minVal = prices[i];
+//             }
+//             dp[i] = Math.max(dp[i-1],prices[i] - minVal);
+//         }
+//         return dp[prices.length-1];
+//     }
+    
+    
+      public int maxProfit(int[] prices) {
+          int min = Integer.MAX_VALUE;
+          int maxProfit = 0;
+          
+          for(int i = 0; i < prices.length;i++){
+              if(min > prices[i]){
+                  min = prices[i];
+              }
+              
+              if(prices[i] - min > maxProfit){
+                maxProfit = prices[i] - min ;
+              }
+          }
+          return maxProfit;
+          
     }
 }
