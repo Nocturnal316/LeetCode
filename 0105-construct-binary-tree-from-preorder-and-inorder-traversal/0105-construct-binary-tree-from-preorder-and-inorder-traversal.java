0c53847435dd6f9da7111608ca3477eb0416b1ad
Nocturnal316 <6785862+Nocturnal316@users.noreply.github.com>
2023-06-12 00:04:06 -0700

    Time: 5 ms (28.51%), Space: 43.2 MB (42.40%) - LeetHub

---
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int startIndx = 0;
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+       
+        return dfs(preorder,inorder,0,preorder.length-1);
+    }
+    
+    public TreeNode dfs(int[] preorder, int[] inorder, int left, int right){
+        if(startIndx == inorder.length  || right < left){
+            return null;
+        }
+        TreeNode root = new TreeNode(preorder[startIndx]);
+        int num = preorder[startIndx];
+        for(int i = 0; i < inorder.length;i++){
+            if(inorder[i] == num){
+                startIndx+=1;
+                root.left = dfs(preorder,inorder,left,i-1);
+                root.right = dfs(preorder,inorder,i+1,right);
+                break;
+            }
+        }
+        
+        
+        return root;
+        
+    }
+}
