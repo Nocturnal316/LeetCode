e167e39e9e718132e3723d3b223565f962476c73
Nocturnal316 <6785862+Nocturnal316@users.noreply.github.com>
2023-06-11 18:32:03 -0700

    Time: 3 ms (46.33%), Space: 41.4 MB (64.69%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -0,0 +1,37 @@
+class Solution {
+    public List<String> ans = new ArrayList<>();
+    public char[][] dict = {{},
+                            {'a','b','c'},
+                            {'d','e','f'},
+                            {'g','h','i'},
+                            {'j','k','l'},
+                            {'m','n','o'},
+                            {'p','q','r','s'},
+                            {'t','u','v'},
+                            {'w','x','y','z'},
+                           };
+    public List<String> letterCombinations(String digits) {
+        if(digits.length() < 1) return ans;
+        dfs(digits,0,new StringBuilder());
+        return ans;
+        
+    }
+    
+    public  void  dfs(String digits, int start, StringBuilder sb){
+        if(start == digits.length()){
+            ans.add(sb.toString());
+            return;
+        }
+         
+        if(start >= digits.length()) return;
+        
+        int num = digits.charAt(start) - '0' - 1;
+        System.out.println(num);
+        for(char c : dict[num]){
+            sb.append(c);
+            dfs(digits,start+1,sb);
+            sb.deleteCharAt(sb.length() -1);
+        }
+       
+    }
+}
