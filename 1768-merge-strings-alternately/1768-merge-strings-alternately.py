42960936c1736283dfeca2bf58f300f2ea3cb71d
Darrel <6785862+Nocturnal316@users.noreply.github.com>
2024-01-23 23:28:05 -0800

    Time: 45 ms (18.04%), Space: 16.5 MB (59.07%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.py
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.py
@@ -1,10 +1,9 @@
 class Solution:
-    def mergeAlternately(self, word1: str, word2: str) -> str:
+    def mergeAlternately2(self, word1: str, word2: str) -> str:
         
         maxLen = len(word1)+len(word2)
         mergedStr = ''
         for i in range(maxLen):
-            
             if len(word1) == 0 or len(word2)== 0:
                 mergedStr += word1
                 mergedStr += word2
@@ -16,5 +15,14 @@ class Solution:
                 mergedStr += word2[0]
                 word2  = word2[1:]
         return mergedStr
-        
+    
+    def mergeAlternately(self,word1: str, word2: str) -> str:
+        merged_str = ''
+        for char1, char2 in zip(word1, word2):
+            merged_str += char1 + char2
+
+        # Append the remaining characters from longer word
+        merged_str += word1[len(word2):] + word2[len(word1):]
+
+        return merged_str
         
